99915d7b0d52e5be84868ad3ad69fea376f75c60
Uran Axel <axel.uran@epfl.ch>
2016-11-19 23:46:30 +0100

    findName finds multiple IDs

--- OptiFood/src/FP.js
+++ OptiFood/src/FP.js
@@ -1,11 +1,15 @@
 var FP = {  "id":{
-              "0":"Tomato",
+              "0":"Banana",
               "1":"Banana",
-              "2":"Orange"
+              "2":"Banana",
+              "3":"Tomato",
+              "4":"Fresh"
             },
-            "name":{
+            "calories":{
               "0":4,
               "1":3,
-              "2":7
+              "2":7,
+              "3":4,
+              "4":5
             }
 }
